063989fe93cd2f9a3a0a3ea23d309f45c578b5df
Erick Ortega <erickortegadev@gmail.com>
2021-10-20 13:26:09 -0500

    10.ModelAndView - GetMapping and Pages path added
    Decidi nombrar cada iteracion por el nombre del video para revisar mejor como se iba desarrolando el proyecto

---
+++ src/main/java/com/cursos/cursospringboot/configuration/Pages.java
@@ -0,0 +1,6 @@
+package com.cursos.cursospringboot.configuration;
+
+public class Pages {
+	public static String HOME = "index";
+	
+}
--- src/main/java/com/cursos/cursospringboot/controllers/ControllerBasic.java
+++ src/main/java/com/cursos/cursospringboot/controllers/ControllerBasic.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.cursos.cursospringboot.configuration.Pages;
 import com.cursos.cursospringboot.models.Post;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,23 +20,23 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/home")
 public class ControllerBasic {
 
-	public List<Post>getPosts(){
+	public List<Post> getPosts() {
 		ArrayList<Post> postList = new ArrayList<>();
 		postList.add(
-				 new Post(
-						  1,
-						  "Lorem ipsum dolor sit amet",
-						  "/img/libro_viejo_abierto.jpg",
-						  "Titulo genial",
-						  new Date()
-				 )
-		);
+				new Post(1, "Lorem ipsum dolor sit amet", "/img/libro_viejo_abierto.jpg", "Titulo genial", new Date()));
 		return postList;
 	}
-	
-	@GetMapping(path={"/post","/"})
+
+	@GetMapping(path = { "/post", "/" })
 	public String saludar(Model model) {
-		model.addAttribute("posts", this.getPosts() );
+		model.addAttribute("posts", this.getPosts());
 		return "index";
+	}
+
+	@GetMapping(path="/public")
+	public ModelAndView post() {
+		ModelAndView modelView = new ModelAndView(Pages.HOME);
+		modelView.addObject("posts", this.getPosts());
+		return modelView;
 	}
 }
